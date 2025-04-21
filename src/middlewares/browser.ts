9e4a0137a39a8bc1ca4130cccae6caa2c63a4793
kav3 <kav3.com@gmail.com>
2025-04-20 21:33:25 -0700

    fix: get browser agent

--- src/middlewares/browser.ts
+++ src/middlewares/browser.ts
@@ -3,8 +3,8 @@ import { MyRequest } from "../types"
 
 export default (req: MyRequest, _: Response, next: NextFunction) => {
     try {
-        const { userAgent } = req.headers ?? {}
-        req.browser = userAgent && /(Mozilla|Chrome|Safari|Opera|Edg|Firefox)/.test(userAgent as string)
+        const userAgent = req.headers['user-agent'] ?? "";
+        req.browser = /mozilla|chrome|safari|firefox|opera|msie|trident/i.test(userAgent.toLowerCase());
     } catch (error) {
         // next(error)
     } finally {
